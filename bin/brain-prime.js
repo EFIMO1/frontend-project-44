4895e0d5466dec74c2e6d1fd3a2464cf52b63f74
EFIMO1 <dragorus53@gmail.com>
2024-03-15 11:26:13 +0300

    1-9 шага

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,7 +4,7 @@ import gameBody from '../src/gameBody.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const task = () => {
-  const num = Math.round(Math.random() * 50);
+  const num = Math.round(Math.random() * 50) + 2;
 
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
